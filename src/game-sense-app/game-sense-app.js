8e17daf4c3a4604eec818659a8ba9e740e4c7e9a
root <root@localhost.localdomain>
2019-03-02 17:05:47 -0600

    It's going well -guess-

--- src/game-sense-app/game-sense-app.js
+++ src/game-sense-app/game-sense-app.js
@@ -2,6 +2,7 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-image/iron-image.js';
 import '@polymer/app-layout/app-layout.js';
 import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-input/paper-input.js';
 /**
  * @customElement
  * @polymer
@@ -10,26 +11,63 @@ class GameSenseApp extends PolymerElement {
   static get template() {
     return html`
       <style>
-        :host {
-          display: block;
-        }
+        
       </style>
-      <center>
-      <app-header>
-        <app-toolbar>
-          <div main-title>Game Sense</div>
-        </app-toolbar>
-      </app-header>
-      <div id="flag-image-container">
-        <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
-        <div id="answer-button-container">
-          <paper-button id="optionA" class="answer">Chi</paper-button>
-          <paper-button id="optionB" class="answer">Ño</paper-button>
+
+
+      <app-header-layout>
+        <app-header reveals effects="waterfall" slot="header">
+          <app-toolbar class="toolbar red">
+            <div main-title>Game Sense</div>
+            <div class="tabs">
+              <a href="#about">About</a>
+              <a href="#services">Services</a>
+              <a href="#contact">Contact</a>
+            </div>
+          </app-toolbar>
+        </app-header>
+        
+        <div class="center">
+            <div class="card">
+                <div id="flag-image-container">
+                    <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
+                        <div id="answer-button-container">
+                            <paper-input always-float-label label="Gamer"></paper-input>
+                            <paper-input always-float-label label="Edad"></paper-input>
+                            <paper-button id="optionA" class="answer">Go</paper-button>
+                        </div>
+                    <paper-button class="another" id="another">Change image</paper-button> 
+                </div>
+            </div>
+        </div>    
+          <br>
+          <br>
+        <div class="padd">
+          <div class="board">
+              <div class="row">
+                <div class="column_2">
+                  <div id="flag-image-container" class="padd">
+                      <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
+                  </div>
+                </div>
+                <div class="column_2 verSyn Gamerinfo">
+                  <div class="">
+                      <br>
+                      <div id="Gamer">Nombre</div>
+                      <br>
+                      <div id="Age">Edad</div>
+                  </div>
+                </div>
+              </div>  
+            <div id="answer-button-container">
+              <paper-input always-float-label label="Gamer"></paper-input>
+              <paper-input always-float-label label="Edad"></paper-input>
+              <paper-button id="optionA" class="answer">Go</paper-button>
+            </div>
+            <paper-button class="another" id="another">Change image</paper-button> 
+          </div>
         </div>
-        <p>A message will go here, telling you if you got it right.</p>
-        <paper-button class="another" id="another">Another!</paper-button> 
-      </div>
-      </center>
+      </app-header-layout>
     
     `;
   }
