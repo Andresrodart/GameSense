18c31d10938fdf8c65d9924082aaaef4d194daec
root <root@localhost.localdomain>
2019-03-02 19:17:41 -0600

    It's going well -guess-

--- build/default/src/game-sense-app/game-sense-app.js
+++ build/default/src/game-sense-app/game-sense-app.js
@@ -2,6 +2,8 @@ import { html, PolymerElement } from "../../node_modules/@polymer/polymer/polyme
 import "../../node_modules/@polymer/iron-image/iron-image.js";
 import "../../node_modules/@polymer/app-layout/app-layout.js";
 import "../../node_modules/@polymer/paper-button/paper-button.js";
+import "../../node_modules/@polymer/paper-input/paper-input.js";
+import Swal from "../../node_modules/sweetalert2/dist/sweetalert2.all.js";
 /**
  * @customElement
  * @polymer
@@ -11,35 +13,100 @@ class GameSenseApp extends PolymerElement {
   static get template() {
     return html`
       <style>
-        :host {
-          display: block;
-        }
+        
       </style>
-      <app-header>
-      <app-toolbar>
-        <div main-title>Game Sense</div>
-      </app-toolbar>
-    </app-header>
-    <div id="flag-image-container">
-      <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
-      <div id="answer-button-container">
-        <paper-button id="optionA" class="answer">Chi</paper-button>
-        <paper-button id="optionB" class="answer">Ño</paper-button>
-      </div>
-      <p>A message will go here, telling you if you got it right.</p>
-      <paper-button class="another" id="another">Another!</paper-button> 
-    </div>
+
+
+      <app-header-layout>
+        <app-header reveals effects="waterfall" slot="header">
+          <app-toolbar class="toolbar red">
+            <div main-title class="center Gamerinfo">Game Sense</div>
+          </app-toolbar>
+        </app-header>
+        
+        <div class="center" id="log">
+            <div class="card">
+                <div id="flag-image-container">
+                    <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
+                        <div id="answer-button-container">
+                            <paper-input always-float-label label="Gamer" id="Gname"></paper-input>
+                            <paper-input always-float-label label="Edad"  id="Gage" ></paper-input>
+                            <paper-button id="optionA" class="answer" on-click="_selectAnswer">[[cA]]</paper-button>
+                        </div>
+                    <paper-button class="another" id="another">[[cB]]</paper-button> 
+                </div>
+            </div>
+        </div>    
+          <br>
+          <br>
+        <div class="padd">
+          <div class="board">
+              <div class="row">
+                <div class="column_2">
+                  <div id="flag-image-container" class="padd">
+                      <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
+                  </div>
+                </div>
+                <div class="column_2 verSyn Gamerinfo">
+                  <div class="">
+                      <br>
+                      <div id="Gamer">[[outpuName]]</div>
+                      <br>
+                      <div id="Age">[[outputAges]]</div>
+                  </div>
+                </div>
+              </div>  
+            <div id="answer-button-container">
+              <paper-input always-float-label label="Gamer"></paper-input>
+              <paper-input always-float-label label="Edad"></paper-input>
+              <paper-button id="optionA" class="answer">Go</paper-button>
+            </div>
+            <paper-button class="another" id="another">Change image</paper-button> 
+          </div>
+        </div>
+      </app-header-layout>
     
     `;
   }
 
   static get properties() {
     return {
-      prop1: {
+      cA: {
         type: String,
-        value: 'game-sense-app'
+        value: "Go!"
+      },
+      cB: {
+        type: String,
+        value: "Iu image"
+      },
+      outpuName: {
+        type: String,
+        value: ""
+      },
+      outputAge: {
+        type: String,
+        value: ""
       }
     };
+  }
+
+  _selectAnswer(event) {
+    let clickedButton = event.target;
+    this.userAnswer = clickedButton.textContent;
+
+    if (this.$.Gname.value != '' && this.$.Gage.value != '') {
+      this.outpuName = this.$.Gname.value;
+      this.outputAge = this.$.Gage.value;
+      this.$.log.hidden = true;
+    } else {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Campos vacios',
+        type: 'error',
+        confirmButtonText: 'Cool'
+      });
+    } //this.$.mein.style.padding = "";
+
   }
 
 }
--- src/game-sense-app/game-sense-app.js
+++ src/game-sense-app/game-sense-app.js
@@ -7,6 +7,7 @@ import '@polymer/paper-input/paper-input.js';
  * @customElement
  * @polymer
  */
+var imgs = ['data/dog.svg', 'data/cow.svg', 'data/pig.svg']
 class GameSenseApp extends PolymerElement {
   static get template() {
     return html`
@@ -15,56 +16,51 @@ class GameSenseApp extends PolymerElement {
       </style>
 
 
-      <app-header-layout>
+      <app-header-layout class="bimg">
         <app-header reveals effects="waterfall" slot="header">
           <app-toolbar class="toolbar red">
-            <div main-title>Game Sense</div>
-            <div class="tabs">
-              <a href="#about">About</a>
-              <a href="#services">Services</a>
-              <a href="#contact">Contact</a>
-            </div>
+            <div main-title class="center Gamerinfo">Game Sense</div>
           </app-toolbar>
         </app-header>
         
-        <div class="center">
+        <div class="center" id="log">
             <div class="card">
                 <div id="flag-image-container">
-                    <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
+                    <iron-image sizing="contain" fade src="[[cImg]]" id="theImg"></iron-image>
                         <div id="answer-button-container">
-                            <paper-input always-float-label label="Gamer"></paper-input>
-                            <paper-input always-float-label label="Edad"></paper-input>
-                            <paper-button id="optionA" class="answer">Go</paper-button>
+                            <paper-input always-float-label label="Gamer" id="Gname"></paper-input>
+                            <paper-input always-float-label label="Edad"  id="Gage" type="number"></paper-input>
+                            <paper-button id="optionA" class="answer" on-click="_selectAnswer">[[cA]]</paper-button>
                         </div>
-                    <paper-button class="another" id="another">Change image</paper-button> 
+                    <paper-button class="another" id="another" on-click="_changeImage">[[cB]]</paper-button> 
                 </div>
             </div>
         </div>    
           <br>
           <br>
-        <div class="padd">
+        <div class="padd" hidden="true" id="Gwork">
           <div class="board">
               <div class="row">
                 <div class="column_2">
                   <div id="flag-image-container" class="padd">
-                      <iron-image sizing="contain" fade src="data/dog.svg"></iron-image>
+                      <iron-image sizing="contain" fade src="[[cImg]]" id="gImage"></iron-image>
                   </div>
                 </div>
                 <div class="column_2 verSyn Gamerinfo">
                   <div class="">
                       <br>
-                      <div id="Gamer">Nombre</div>
+                      <div id="Gamer">[[outpuName]]</div>
                       <br>
-                      <div id="Age">Edad</div>
+                      <div id="Age">[[outputAge]]</div>
                   </div>
                 </div>
               </div>  
             <div id="answer-button-container">
               <paper-input always-float-label label="Gamer"></paper-input>
               <paper-input always-float-label label="Edad"></paper-input>
-              <paper-button id="optionA" class="answer">Go</paper-button>
             </div>
-            <paper-button class="another" id="another">Change image</paper-button> 
+            <br>
+            <paper-button class="another" id="another" on-click="_exit" >Exit</paper-button> 
           </div>
         </div>
       </app-header-layout>
@@ -73,11 +69,56 @@ class GameSenseApp extends PolymerElement {
   }
   static get properties() {
     return {
-      prop1: {
+      cA: {
         type: String,
-        value: 'game-sense-app'
+        value: "Go!"
+      },
+      cB: {
+        type: String,
+        value: "Iu image"
+      },cImg: {
+        type: String,
+        value: imgs[Math.floor(Math.random() * 3)]
+      },
+      outpuName: {
+        type: String,
+        value: "Nombre: "
+      },
+      outputAge: {
+        type: String,
+        value: "Edad: "
       }
     };
+  }
+  _selectAnswer(event) {
+    let clickedButton = event.target;
+    this.userAnswer = clickedButton.textContent;
+    if (this.$.Gname.value != '' && this.$.Gage.value != '') {
+      this.outpuName += this.$.Gname.value;
+      this.outputAge += this.$.Gage.value;
+      this.$.log.hidden = true;
+      this.$.Gwork.hidden = false;
+    }else{
+      Swal.fire({
+      title: 'Error!',
+      text: 'Campos vacios',
+      type: 'error',
+      confirmButtonText: 'Cool'
+      })
+    }
+    //this.$.mein.style.padding = "";
+  }
+  _changeImage(event) {
+    this.cImg = (imgs.indexOf(this.cImg) + 2 > imgs.length)? imgs[0] : imgs[imgs.indexOf(this.cImg) + 1]
+    this.$.theImg.setAttribute('src', this.cImg)
+    this.$.gImage.setAttribute('src', this.cImg)
+  }
+
+  _exit(event){
+    this.$.Gname.value = '';
+    this.$.Gage.value = '';
+    this.$.Gwork.hidden = true;
+    this.$.log.hidden = false;
   }
 }
 
