dbb87ab17456acb2a9fac8aa7489112895af836f
Manu Utrera <tst424@vm.cut>
2019-03-03 00:19:56 -0600

    commit del JSON server

--- biodata/scaner.py
+++ biodata/scaner.py
@@ -1,15 +1,15 @@
 import time
 from bitalino import BITalino
-
+import random
 # The macAddress variable on Windows can be "XX:XX:XX:XX:XX:XX" or "COMX"
 # while on Mac OS can be "/dev/tty.BITalino-XX-XX-DevB" for devices ending with the last 4 digits of the MAC address or "/dev/tty.BITalino-DevB" for the remaining
 macAddress = "20:16:02:26:60:31"
 
 # This example will collect data for 5 sec.
-running_time = 5
+running_time = 10
     
-batteryThreshold = 30
-acqChannels = [0, 1, 2, 3, 4, 5]
+batteryThreshold = 60
+acqChannels = [0, 2, 4]
 samplingRate = 1000
 nSamples = 10
 digitalOutput = [1,1]
@@ -28,9 +28,19 @@ device.start(samplingRate, acqChannels)
 
 start = time.time()
 end = time.time()
+dict_ = {
+    'samble':'',
+    'channel 1':'',
+    'channel 3':'',
+    'channel 5':''
+}
+arr_to_json = {}
 while (end - start) < running_time:
     # Read samples
-    print(device.read(nSamples))
+    arr_channels = device.read(nSamples)
+    
+    arr_to_json
+    print(, end='\n\n')
     end = time.time()
 
 # Turn BITalino led on
